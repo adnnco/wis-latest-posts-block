96ad2cfa68b2f98b4a714b072d8fe1b9524f7618
Adnan Günay <adnnco@gmail.com>
2024-11-22 19:16:43 +0100

    block structure created

--- src/edit.js
+++ src/edit.js
@@ -1,41 +1,42 @@
-/**
- * Retrieves the translation of text.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
- */
 import { __ } from '@wordpress/i18n';
+import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
+import { PanelBody, RangeControl } from '@wordpress/components';
+import { useEffect, useState } from 'react';
 
-/**
- * React hook that is used to mark the block wrapper element.
- * It provides all the necessary props like the class name.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
- */
-import { useBlockProps } from '@wordpress/block-editor';
+export default function Edit({ attributes, setAttributes }) {
+	const { numberOfPosts } = attributes;
+	const [posts, setPosts] = useState([]);
 
-/**
- * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
- * Those files can contain any CSS code that gets applied to the editor.
- *
- * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
- */
-import './editor.scss';
+	useEffect(() => {
+		fetch(`/wp-json/wis/v1/latest-posts?per_page=${numberOfPosts}`)
+			.then((response) => response.json())
+			.then((data) => setPosts(data));
+	}, [numberOfPosts]);
 
-/**
- * The edit function describes the structure of your block in the context of the
- * editor. This represents what the editor will render when the block is used.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
- *
- * @return {Element} Element to render.
- */
-export default function Edit() {
 	return (
-		<p { ...useBlockProps() }>
-			{ __(
-				'Wis Latest Posts Block – hello from the editor!',
-				'wis-latest-posts-block'
-			) }
-		</p>
+		<>
+			<InspectorControls>
+				<PanelBody title={__('Settings', 'wis-latest-posts-block')}>
+					<RangeControl
+						label={__('Number of Posts', 'wis-latest-posts-block')}
+						value={numberOfPosts}
+						onChange={(value) => setAttributes({ numberOfPosts: value })}
+						min={1}
+						max={10}
+					/>
+				</PanelBody>
+			</InspectorControls>
+			<div {...useBlockProps()}>
+				{posts.map((post) => (
+					<div key={post.id}>
+						<h2>{post.title}</h2>
+						<div dangerouslySetInnerHTML={{ __html: post.excerpt }} />
+						{post.featured_image && (
+							<img src={post.featured_image} alt={post.title} />
+						)}
+					</div>
+				))}
+			</div>
+		</>
 	);
 }
--- wis-latest-posts-block.php
+++ wis-latest-posts-block.php
@@ -27,4 +27,37 @@ if ( ! defined( 'ABSPATH' ) ) {
 function create_block_wis_latest_posts_block_block_init() {
 	register_block_type( __DIR__ . '/build' );
 }
+
 add_action( 'init', 'create_block_wis_latest_posts_block_block_init' );
+
+
+function wis_register_rest_routes(): void {
+	register_rest_route( 'wis/v1', '/latest-posts', array(
+		'methods'  => 'GET',
+		'callback' => 'wis_get_latest_posts',
+	) );
+}
+
+function wis_get_latest_posts( $data ): WP_REST_Response {
+	$args = array(
+		'numberposts' => 5, // You can change this to the number of posts you want to retrieve
+		'post_status' => 'publish',
+	);
+
+	$posts = get_posts( $args );
+	$data  = array();
+
+	foreach ( $posts as $post ) {
+		$featured_image_url = get_the_post_thumbnail_url( $post->ID, 'full' );
+		$data[]             = array(
+			'id'             => $post->ID,
+			'title'          => get_the_title( $post->ID ),
+			'excerpt'        => get_the_excerpt( $post->ID ),
+			'featured_image' => $featured_image_url,
+		);
+	}
+
+	return new WP_REST_Response( $data, 200 );
+}
+
+add_action( 'rest_api_init', 'wis_register_rest_routes' );
